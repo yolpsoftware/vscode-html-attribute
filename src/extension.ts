de0f0882371270ed9830136869c8e060dee62133
Jonas Sourlier <jonas.sourlier@yolp.ch>
2021-10-26 16:33:39 +0200

    developed extension, but edits do not appear in the editor

--- src/extension.ts
+++ src/extension.ts
@@ -5,21 +5,65 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-html-attribute" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-html-attribute.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-html-attribute!');
-	});
+    context.subscriptions.push(
+        vscode.commands.registerTextEditorCommand('vscode-html-attribute.insertAltFromClipboard',
+            (textEditor, edit, args) => {
+                return closeSelections(textEditor, edit, args);
+            }
+        )
+    );
+}
 
-	context.subscriptions.push(disposable);
+const closeSelections = async (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, args: any) => {
+
+    await Promise.all(textEditor.selections.map(async selection => {
+        const startLine = selection.start.line;
+        const startCharacter = selection.start.character;
+        const altText = await vscode.env.clipboard.readText();
+        if (!altText) {
+            vscode.window.showWarningMessage('No text in clipboard');
+            return;
+        }
+        const lineText = textEditor.document.getText(new vscode.Range(startLine, 0, startLine, textEditor.document.lineAt(startLine).range.end.character));
+        const index = lineText.indexOf('<img');
+        if (index === -1) {
+            vscode.window.showWarningMessage('No <img> tag found on this line.');
+            return;
+        }
+        if (lineText.indexOf('<img', index + 1) > -1) {
+            vscode.window.showWarningMessage('Found more than one <img> tag.');
+            return;
+        }
+        const srcIndex = lineText.indexOf('src="', index);
+        if (srcIndex === -1) {
+            // append at end of line
+            await edit.insert(new vscode.Position(startLine, lineText.length - 1), ` alt="${altText}"`);
+        } else {
+            const srcEndIndex = lineText.indexOf('"', srcIndex + 5);
+            await edit.insert(new vscode.Position(startLine, srcEndIndex), ` alt="${altText}"`);
+        }
+
+        //vscode.workspace.applyEdit(edit);
+        /*const tagToClose = findTags(() => {
+            if (startLine < 0) {
+                return;
+            } else {
+                const line = getLineText(textEditor, startLine--, startCharacter);
+                startCharacter = Infinity;
+                return line;
+            }
+        }, { ignoreTags });
+        if (tagToClose) {
+            const closeTag = `</${ tagToClose }>`;
+
+            if (selection.start.line !== selection.end.line || selection.start.character !== selection.end.character) {
+                edit.replace(selection, closeTag);
+            } else {
+                edit.insert(selection.anchor, closeTag);
+            }
+        }*/
+    }));
 }
 
 // this method is called when your extension is deactivated
